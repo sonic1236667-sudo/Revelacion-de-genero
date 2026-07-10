37c30cae15ed603f92db3ee27719271d4e8ab5d8
sonic1236667-sudo <sonic1236667@gmail.com>
2026-07-10 12:17:40 -0500

    Add files via upload

--- rs.js
+++ rs.js
@@ -1,3 +1,8 @@
 function abrirUbicacion() {
   window.location.href = "https://maps.google.com";
 }
+const musica = document.getElementById("musica");
+
+document.addEventListener("click", () => {
+    musica.play();
+}, { once: true });
